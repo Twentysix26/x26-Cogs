56bef999fbdca9b35a4a4272be28fe278104b5d0
Fixator10 <github@fixator10.ru>
2020-09-18 18:18:32 +0400

    Make every single discord.Member converter consume-rest

--- defender/defender.py
+++ defender/defender.py
@@ -213,7 +213,7 @@ class Defender(commands.Cog):
         return em
 
     @defender.command(name="identify")
-    async def defenderidentify(self, ctx, user: discord.Member):
+    async def defenderidentify(self, ctx, *, user: discord.Member):
         """Shows a member's rank + info"""
         em = await self.make_identify_embed(ctx.message, user)
         await ctx.send(embed=em)
@@ -1187,7 +1187,7 @@ class Defender(commands.Cog):
 
     @commands.cooldown(1, 22, commands.BucketType.guild)  # More useful as a lock of sorts in this case
     @commands.command(cooldown_after_parsing=True)        # Only one concurrent session per guild
-    async def voteout(self, ctx, user: discord.Member):
+    async def voteout(self, ctx, *, user: discord.Member):
         """Initiates a vote to expel a user from the server
 
         Can be used by members with helper roles during emergency mode"""
