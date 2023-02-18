aba62cdafc373cf53e2c2c3388c1971eeba0304c
Twentysix <Twentysix26@users.noreply.github.com>
2023-02-18 13:38:10 +0100

    [Def] Await cog package / remove 3.5 safeguards

--- defender/__init__.py
+++ defender/__init__.py
@@ -7,8 +7,5 @@ with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 
-def setup(bot):
-    if version_info >= VersionInfo.from_str("3.5.0"):
-        raise RuntimeError("Defender needs to be updated to run on Red 3.5.0")
-
-    bot.add_cog(Defender(bot))
+async def setup(bot):
+    await bot.add_cog(Defender(bot))
