3a950b780eb88aa4abbfe16d7d56a0cfa8197056
Twentysix <Twentysix26@users.noreply.github.com>
2024-06-07 22:06:45 +0200

    [Def] Version bump

--- defender/defender.py
+++ defender/defender.py
@@ -121,7 +121,7 @@ default_owner_settings = {
 class Defender(Commands, AutoModules, Events, commands.Cog, metaclass=CompositeMetaClass):
     """Security tools to protect communities"""
 
-    __version__ = "2.0.1"
+    __version__ = "v2.1.1"
 
     def __init__(self, bot):
         self.bot = bot
