b4a84bca49f16af99767e077264d60689dc5c9d6
AAA3A <89632044+AAA3A-AAA3A@users.noreply.github.com>
2024-07-28 20:08:21 +0200

    Update warden.py

--- defender/dashboard/warden.py
+++ defender/dashboard/warden.py
@@ -150,7 +150,7 @@ class WardenIntegration:
 WEB_CONTENT = """
     <div class="alert alert-{{ "success" if warden_enabled else "danger" }} text-white d-flex justify-content-between" role="alert">
         <p style="padding-top: 10px;">Warden is currently <strong>{{ "enabled" if warden_enabled else "disabled" }}</strong>.</p>
-        <a href="{{ url_for("third_parties_blueprint.third_party", name=name, page="settings", guild_id=guild.id) }}" class="btn btn-gradient-default text-white">View Settings</a>
+        <a href="{{ url_for("third_parties_blueprint.third_party", name=name, page="settings", guild_id=guild.id, _anchor="Warden") }}" class="btn btn-gradient-default text-white">View Settings</a>
     </div>
 
     {{ warden_rules_form|safe }}
